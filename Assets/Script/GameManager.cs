c56728a5f61d7549ef80c6cbfef9045166a53e47
Efe Hascan <serefefehascan@gmail.com>
2024-12-19 00:17:30 +0300

    3D last

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,10 +1,12 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private int _widht = 4;
     [SerializeField] private int _height = 4;
-    [SerializeField] public Node _nodePrefeb;
+    [SerializeField] public Node nodePrefeb;
+    [SerializeField] public SpriteRenderer boardPrefeb;
 
     private void Start()
     {
@@ -17,8 +19,12 @@ public class GameManager : MonoBehaviour
         {
             for (int y = 0; y < _height; y++)
             {
-                var node = Instantiate(_nodePrefeb, new Vector2(x,y), Quaternion.identity);
+                var node = Instantiate(nodePrefeb, new Vector2(x,y), Quaternion.identity);
             }
         }
+        var center = new Vector2((float) _widht /2 - 0.5f,(float) _height /2 - 0.5f);
+
+        var board = Instantiate(boardPrefeb, Vector3.zero, Quaternion.identity);
+
     }
 }
